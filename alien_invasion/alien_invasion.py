7f26cea1ae0d944c90752d310021d2850c9774c8
anubhavpatrick <anubhavpatrick@gmail.com>
2021-11-11 16:56:14 +0530

    Added settings module

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,8 +1,8 @@
 '''The main module for the game'''
 
 import sys
-
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     "Master class to manage game play and assets"
@@ -11,11 +11,12 @@ class AlienInvasion:
         """Initializa the game,create game resources"""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200,800)) #surface created
+        #create settings object
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_height, self.settings.screen_width)) #surface created
 
         pygame.display.set_caption("Alien Invasion by Anubhav")
-
-        self.bg_color = (230,230,230)
 
 
     def run_game(self):
@@ -25,7 +26,7 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:#when user closes the window
                     sys.exit()
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             #Make the most recently drawn screen visible
             pygame.display.flip()
---
+++ alien_invasion/settings.py
@@ -0,0 +1,11 @@
+'''A module that stores and updates all the game settings'''
+
+class Settings:
+    '''A class to store all the settings of the game'''
+
+    def __init__(self):
+        '''Initialize the default settings of the game'''
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
